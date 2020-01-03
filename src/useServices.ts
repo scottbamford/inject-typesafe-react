55c81b3a27dde3086cd79557aad6a3f3a7023b96
Scott Bamford <scott@ambidect.company>
2020-01-03 15:20:46 +0000

    useServices() now uses useMemo() by default on its results and allows an optional list of additional dependencies to be passed in that will be passed on to useMemo() along with the serviceProvider.

--- src/useServices.ts
+++ src/useServices.ts
@@ -11,17 +11,19 @@ import { ServiceProviderContext } from "./ServiceProviderContext";
  * 
  * Please be careful when using this that you don't end up using it as a service locator and loosing the benefits of constructor/props based dependency injection.
  * Consider use of withServiceProps() HOC if you want to ensure all dependencies are props of your component.
- * @param Container
- * @param props
  */
-export function useServices<T = any, ServiceCollection = any>(resolve: (services: ServiceCollection) => T): T {
+export function useServices<T = any, ServiceCollection = any>(resolve: (services: ServiceCollection) => T, deps: Array<any> = []): T {
     let serviceProvider = React.useContext(ServiceProviderContext);
-    if (!serviceProvider) {
-        throw 'You must include a parent <ServiceProviderContext.Provider> in your component tree to use the withServiceProps HOC or useServices() hook.'
-    }
 
-    // Each injection takes place as a seperate scope.
-    let scope = serviceProvider.createScope();
+    // Resolve the services and store the results using useMemo to optimise rendering.
+    return React.useMemo(() => {
+        if (!serviceProvider) {
+            throw 'You must include a parent <ServiceProviderContext.Provider> in your component tree to use the withServiceProps HOC or useServices() hook.'
+        }
 
-    return resolve(scope.services());
+        // Each injection takes place as a seperate scope.
+        let scope = serviceProvider.createScope();
+
+        return resolve(scope.services());
+    }, [serviceProvider, ...deps]);
 }
